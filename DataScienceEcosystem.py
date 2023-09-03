274dc4080ff15d2d28aaa65e253a2fb140122654
albacabanillas <143934826+albacabanillas@users.noreply.github.com>
2023-09-03 19:47:10 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives:**
+# - List some languages or libraries for Data Science
+# - Insert and delete cells in Jupyter Notebooks
+# - Insert table
+# - Use some arithmetic expressions for Python
+
+# Some of the popular languages that Data Scientists use are:  
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientist include: 
+# 1. TensorFlow
+# 2. Pandas
+# 3. SciKit-Learn
+# 4. Pytorch
+
+# In[8]:
+
+
+#Exercise 6
+
+
+# 
+# | Data Science Tools       |
+# |--------------------------|
+# | R studio                |
+# | Apache Spark                   |
+# | Tensor Flow    
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[4]:
+
+
+a = 1
+b = 2
+ 
+c = a + b
+d = a * b
+f = d/2
+
+print (c)
+print (d)
+print (f)
+
+
+# In[5]:
+
+
+#Exercise 8
+
+#This a simple arithmetic expression to mutiply then add integers
+result= (3*4)+5
+print (result)
+
+
+# In[7]:
+
+
+#Excercise 9
+
+#This will convert 200 minutes to hours by diving by 60
+min = 200
+hours = 200 / 60
+print (hours)
+
+
+# ## Alba Cabanillas Pascual
+
+# In[ ]:
+
+
+
+
